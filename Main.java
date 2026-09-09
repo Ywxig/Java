22ef0ee207100f3c2b3a37e6a4aa124894dacd31
Ywxig <d.zimnov@gmail.com>
2026-09-09 16:29:23 +0300

    feat: create a dir for lab-1

--- Main.java
+++ Main.java
@@ -1,49 +1,52 @@
 class Student {
-	String name;    //
-	Float lazynass; // коэфициент лени от 0-1 где 1 это ленивый
-	Byte IQ;        // (максимальное значение 127) 80-127
-	int exams;      // кол-во экзаменов
-	int marks[];     // кол-во оценок
+	String name;
+	float laziness; // примитивный тип вместо Float
+	byte iq;        // примитивный тип вместо Byte
+	int exams;
+	int[] marks;    // стиль Java: тип[] имяПеременной
+	static int nzStuck;
 
-	Student () {    // конструктор класса студент
+	Student() {
 		name = "Pantera";
-		lazynass = 0.67f; // литерал f для float
-		IQ = (byte)127;
+		laziness = 0.67f;
+		iq = 127;
 		exams = 6;
 		marks = new int[exams];
-		for(int i = 0; i < exams; i++) {
+		for (int i = 0; i < exams; i++) {
 			marks[i] = 7;
 		}
+		nzStuck++;
 	}
 
-	Student (Float lazynass, Byte iq) { // конструктор с параметром
+	Student(float laziness, byte iq) {
 		name = "Erick";
-		this.lazynass = lazynass; // чтобы небыло конфликта надо использовать this
-		IQ = iq; // конфликта имён не будет так как разные регистры
+		this.laziness = laziness;
+		this.iq = iq;
 		exams = 18;
 		marks = new int[exams];
-		for(int i = 0; i < exams; i++) {
+		for (int i = 0; i < exams; i++) {
 			marks[i] = 8;
 		}
+		nzStuck++;
 	}
 
-	Student (Student other) {    // конструктор копирования
+	Student(Student other) {
 		name = other.name;
-		lazynass = other.lazynass;
-		IQ = other.IQ;
+		laziness = other.laziness;
+		iq = other.iq;
 		exams = other.exams;
 		marks = new int[exams];
-		for(int i = 0; i < exams; i++) {
+		for (int i = 0; i < exams; i++) {
 			marks[i] = other.marks[i];
 		}
+		nzStuck++;
 	}
 
 	void echo() {
 		System.out.println("name: " + this.name);
-		System.out.println("lazynass: " + this.lazynass);
-		System.out.println("IQ: " + this.IQ);
+		System.out.println("laziness: " + this.laziness);
+		System.out.println("iq: " + this.iq);
 		System.out.println("exams: " + this.exams);
-		// вывод массива оценок
 		System.out.print("marks:");
 		for (int i = 0; i < exams; i++) {
 			System.out.print(" " + this.marks[i]);
@@ -51,39 +54,44 @@ class Student {
 		System.out.println("\n");
 	}
 
-	public static void main(String[] furry) { // Главная точка входа программы
- 	//     // 1. Создание экземпляра через конструктор по умолчанию
- 	//     Student vasia = new Student();
-	 //    vasia.echo();
+	public static void main(String[] args) {
+		int studentCount = 18;
+		Student[] students = new Student[studentCount];
 
-	 //    // 2. Создание экземпляра через конструктор с параметрами
-	 //    Student erick = new Student(0.45f, (byte)110);
-	 //    erick.echo();
-
-	 //    // 3. Создание экземпляра через конструктор копирования
-	 //    Student vasiaCopy = new Student(vasia);
-	 //    vasiaCopy.echo();
-
-		// //4. Привемр из айдитории
-		// Student Artemii;
-		// Artemii = new Student();
-		// Artemii.echo();
-
-		// создадим через цикол несколько 18 студентов
-		// будем сохранять из в массив
-
-		int student_count = 18;
-		Student[] students = new Student[student_count];
-		for (int i = 0; i < student_count; i+=3) {
+		for (int i = 0; i < studentCount; i += 3) {
 			students[i] = new Student();
-			students[i+1] = new Student((i * 0.01f), (byte)(100+i));
-			students[i+2] = new Student(students[i]);
+			students[i + 1] = new Student((i * 0.01f), (byte) (100 + i));
+			students[i + 2] = new Student(students[i]);
 		}
 
-		// вывод информации о студентах
-		for (int i = 0; i < student_count; i++) {
-			students[i].echo();
+		// // Вывод информации о студентах
+		// for (int i = 0; i < studentCount; i++) {
+		// 	students[i].echo();
+		// }
+
+		// 1-й способ поиска студента с наименьшим IQ
+		byte lowestIq = students[0].iq;
+		int lowestIqIndex = 0;
+		for (int i = 1; i < studentCount; i++) {
+			if (students[i].iq < lowestIq) {
+				lowestIq = students[i].iq;
+				lowestIqIndex = i;
+			}
 		}
+
+		System.out.println("Lowest IQ: " + students[lowestIqIndex].iq + " at index " + lowestIqIndex);
+		students[lowestIqIndex].echo();
+
+		// 2-й способ поиска (по объекту)
+		Student charle = students[0];
+		for (int i = 1; i < students.length; i++) {
+			if (students[i].iq < charle.iq) {
+				charle = students[i];
+			}
+		}
+		System.out.println("Charle Leclerc:");
+		charle.echo();
+
+		System.out.println("было созданно студентов: " + Student.nzStuck);
 	}
-
 }
